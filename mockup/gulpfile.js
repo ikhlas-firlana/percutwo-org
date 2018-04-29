a7f2c664287ba11420655add1d208da29c35a12e
ikhlas firlana <ikhlas.firlana@gmail.com>
2018-04-29 09:30:19 +0700

    how to make node_modules/ goes to index.html when make llive reloading

--- mockup/gulpfile.js
+++ mockup/gulpfile.js
@@ -1,9 +1,18 @@
 var gulp = require('gulp');
 var ejs = require("gulp-ejs");
 var log = require('fancy-log');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function() {
-    return gulp.src('src/templates/*.ejs')
+gulp.task('default', ['ejs'], function() {
+    browserSync.init({
+        server: "./dist"
+    });
+
+    gulp.watch("src/**/*.ejs", ['ejs', browserSync.reload]);
+});
+
+gulp.task('ejs', function () {
+    return gulp.src('src/*.ejs')
         .pipe(ejs({}, {}, {ext:'.html'})).on('error', log)
         .pipe(gulp.dest('dist/'))
 });
